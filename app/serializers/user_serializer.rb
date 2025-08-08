da6f3be4e0e7f324b6e3f36ddb2ab2929b663665
Eka PD <edharmowongso@gmail.com>
2025-08-08 17:32:16 +0700

    feat(user): add total_followers & total_following
    Signed-off-by: Eka PD <edharmowongso@gmail.com>

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -6,4 +6,12 @@ class UserSerializer
   attribute :created_at do |record|
     record.created_at.strftime(DATETIME_FORMAT)
   end
+  
+  attribute :total_followers do |record|
+    record.followers.count
+  end
+  
+  attribute :total_following do |record|
+    record.following.count
+  end
 end
